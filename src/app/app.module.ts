dfa07e5c67a6bea7732070bb1ebfd6474c7d40bf
jp49283 <pjorkowski01@gmail.com>
2023-11-20 16:01:01 +0100

    lab-a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {provideRouter, RouterModule} from "@angular/router";
+import routeConfig from "./routes";
 
 import { AppComponent } from './app.component';
+import {HomeComponent} from "./home/home.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomeComponent,
+    RouterModule,
   ],
-  providers: [],
+  providers: [provideRouter(routeConfig)],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
